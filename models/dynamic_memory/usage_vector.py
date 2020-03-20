9b97948f84f2dc01ab3a835b8e20e6255a1e6df3
Zan Huang <zan@Zans-MBP.fios-router.home>
2020-03-20 17:59:55 -0400

    fixed memory_retention_vector for potential bugs

--- models/dynamic_memory/usage_vector.py
+++ models/dynamic_memory/usage_vector.py
@@ -2,4 +2,12 @@ import tensorflow
 import numpy as np
 
 def usage_vector(memory_retention_vector, prev_usage_vector, prev_write_vector):
-    print("test")
+    assert prev_write_vector[:][0].shape == prev_usage_vector[0][:].shape #the write vector should be the
+    #same size as the usage vector in that dimension.
+    vector_before_retention = (prev_usage_vector + prev_write_vector - np.multiply(prev_usage_vector, prev_write_vector))
+    memory_retention_vector = np.transpose(memory_retention_vector)
+    final_vector = np.multiply(vector_before_retention, memory_retention_vector)
+    return final_vector
+    #notes: essentially, the usage vector should always return a size of (n*1),
+    #which is why the memory retention vector is always transposed, since its initially
+    #it is of size (1*n).
