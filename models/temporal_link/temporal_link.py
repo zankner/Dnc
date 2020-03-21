30b705616bcbb3e19f34be4308a64a2e6c6d9321
Zack Ankner <zackankner@gmail.com>
2020-03-20 21:30:20 -0400

    feat(temporal-linking) made temporal link function and not tf layer

--- models/temporal_link/temporal_link.py
+++ models/temporal_link/temporal_link.py
@@ -1,21 +1,9 @@
 import tensorflow as tf
-# Import layers:
-from tensorflow.keras.layers import (
-    Dense, Flatten, Conv2D, Conv2DTranspose, 
-    BatchNormalization, concatenate, MaxPool2D
-)
 from tensorflow.keras.layers import Layer
 
 #Defining temporal linking Below:
-class TemporalLinking(Layer):
-  def __init__(self):
-    super(TemporalLinking, self).__init__()
-    # Define layers of the network:
-
-  def call(self, x, training=False):
-    # Call layers of network on input x
-    # Use the training variable to handle adding layers such as Dropout
-    # and Batch Norm only during training
-    precedence = weight_precedence(precedence, write_weight)
-
-    return x
+def temporal_link(tempral_links, precedence, write_weightings):
+  temporal_links = update_temporal_links(
+      temporal_links, write_weightings, precedence)
+  precedence = weight_precedence(precedence, write_weightings)
+  return temporal_links, precedence 
