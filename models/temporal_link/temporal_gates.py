bb750ff3bb0fb22d9f56e70e6df45c063ddf0731
Zack Ankner <zackankner@gmail.com>
2020-03-20 21:46:58 -0400

    fix(temporal-linking) refactor -> rename temporal_gates to temporal_weights

--- models/temporal_link/temporal_gates.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-def temporal_gates(temporal_links, write_weights):
-  forward_gate = tf.matmul(write_weights, 
-      temporal_links, transpose_b = True)
-  backward_gate = tf.matmul(write_weights, temporal_links)
-  return forward_gate, backward_gate
