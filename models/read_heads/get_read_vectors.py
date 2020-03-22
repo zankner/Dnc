8bcc09e93155063792910161cb8f49261791b036
mikhaildmitrienko <michael.dmitrienko@gmail.com>
2020-03-22 14:48:17 -0400

    fix(mikhail) changed from elementwise to dot product

--- models/read_heads/get_read_vectors.py
+++ models/read_heads/get_read_vectors.py
@@ -1,4 +1,4 @@
 import tensorflow as tf
 
 def get_read_vectors(memory_matrix, read_weightings):
-    return tf.math.multiply(tf.transpose(memory_matrix), read_weightings)
+    return tf.linalg.matmul(tf.transpose(memory_matrix), read_weightings)
