54450590a45b72515b7f073666c59d87832a8b33
mikhaildmitrienko <michael.dmitrienko@gmail.com>
2020-03-22 14:48:17 -0400

    fix(mikhail) changed from elementwise to dot product

--- models/write_heads/write.py
+++ models/write_heads/write.py
@@ -6,5 +6,5 @@ def write(memory_matrix, write_weighting, erase_vector, write_vector):
     write_vector = tf.transpose(write_vector)
     big_e = tf.ones(tf.shape(m).numpy())
 
-    memory_matrix = tf.math.multiply(memory_matrix, tf.math.add(tf.math.subtract(big_e, tf.math.multiply(write_weighting, erase_vector)), tf.math.multiply(write_weighting, write_vector)))
+    memory_matrix = tf.math.multiply(memory_matrix, tf.math.add(tf.math.subtract(big_e, tf.linalg.matmul(write_weighting, erase_vector)), tf.linalg.matmul(write_weighting, write_vector)))
     return memory_matrix
