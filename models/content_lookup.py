fdefe7411ab7d3e027c1bc3cbed9fa58c8d3bdd8
mikhaildmitrienko <michael.dmitrienko@gmail.com>
2020-03-22 19:10:09 -0400

    feat(mikhail) added content lookup function

---
+++ models/content_lookup.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+def content_lookup(memory_matrix, key, key_strength, row):
+
+    def cosine_sim(u, v):
+        return tf.math.divide(tf.linalg.mathmul(u, v), tf.linalg.mathmul(tf.math.abs(u), tf.math.abs(v))
+
+    key = tf.transpose(key)
+    return tf.nn.softmax(cosine_sim(key, memory_matrix) * key_strength)
