125c054edbae2c117f159dc49d5fbdcca10949c9
mikhaildmitrienko <michael.dmitrienko@gmail.com>
2020-03-22 19:15:04 -0400

    fix(mikhail) remove unused param

--- models/content_lookup.py
+++ models/content_lookup.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-def content_lookup(memory_matrix, key, key_strength, row):
+def content_lookup(memory_matrix, key, key_strength):
 
     def cosine_sim(u, v):
         return tf.math.divide(tf.linalg.mathmul(u, v), tf.linalg.mathmul(tf.math.abs(u), tf.math.abs(v))
