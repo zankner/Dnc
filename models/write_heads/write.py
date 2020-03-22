ae350fd306d1c37577051f999fa9c095c835966d
mikhaildmitrienko <michael.dmitrienko@gmail.com>
2020-03-22 15:21:57 -0400

    fix(mikhail) renamed ambiguous var

--- models/write_heads/write.py
+++ models/write_heads/write.py
@@ -4,7 +4,7 @@ def write(memory_matrix, write_weighting, erase_vector, write_vector):
 
     erase_vector = tf.transpose(erase_vector)
     write_vector = tf.transpose(write_vector)
-    big_e = tf.ones(tf.shape(m).numpy())
+    big_e = tf.ones(tf.shape(memory_matrix).numpy())
 
     memory_matrix = tf.math.multiply(memory_matrix, tf.math.add(tf.math.subtract(big_e, tf.linalg.matmul(write_weighting, erase_vector)), tf.linalg.matmul(write_weighting, write_vector)))
     return memory_matrix
