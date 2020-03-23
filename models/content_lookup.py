e77df37ee583301d8b0088b6e079cb3e237fbd04
mikhaildmitrienko <michael.dmitrienko@gmail.com>
2020-03-22 20:16:15 -0400

    fix(mikhail) changed from abs val to norm

--- models/content_lookup.py
+++ models/content_lookup.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 def content_lookup(memory_matrix, key, key_strength):
 
     def cosine_sim(u, v):
-        return tf.math.truediv(tf.linalg.matmul(u, v), tf.linalg.matmul(tf.math.abs(u), tf.math.abs(v)))
+        return tf.math.truediv(tf.linalg.matmul(u, v), tf.math.multiply(tf.norm(u), tf.norm(v)))
 
     key = tf.transpose(key)
 
