c86628b29728d6ddc741e6407e705afe321513ad
Zan Huang <www.zandhuang@gmail.com>
2020-03-24 00:58:18 -0400

    fixed sorting indices

--- models/dynamic_memory/sorting_indices.py
+++ models/dynamic_memory/sorting_indices.py
@@ -9,8 +9,12 @@ def sorting_indices(usage_vector):
 
     free_list_sorted = tf.argsort(free_list_unsorted, axis = 0, direction = 'ASCENDING', stable=True)
     free_list_sorted_index = tf.keras.backend.eval(free_list_sorted)
-    print(free_list_sorted_index)
+    #free_list_sorted_index = tf.transpose(free_list_sorted_index)
+    #print(free_list_sorted_index)
     ordered_memory_usage = tf.gather(unsorted_memory_usage, free_list_sorted_index, axis=0)
-    print(ordered_memory_usage)
+    #print(ordered_memory_usage)
+    ordered_memory_usage = tf.linalg.diag_part(ordered_memory_usage)
+    #step to fetch the eye of the Matrix
+
 
     return ordered_memory_usage
