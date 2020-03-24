ce1dcb3d3d7388f2e869d5ef9b36b9e65ca2e0e1
Zan Huang <www.zandhuang@gmail.com>
2020-03-24 01:56:17 -0400

    debugged and fixed write_weight_interpolation

--- models/dynamic_memory/write_weight_interpolation.py
+++ models/dynamic_memory/write_weight_interpolation.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def write_weight_interpolation(allocation_gate, write_gate, allocation_vector, content_vector):
     allocation_term = allocation_gate * allocation_vector
-    content_term = (tf.ones(tf.shape(allocagtion_gate).numpy()) - allocation_gate) * content_vector
-    write_information = allocation_term + content_term
+    content_term = (tf.cast(tf.ones(tf.shape(allocation_gate).numpy()), tf.float64) - allocation_gate) * content_vector
+    write_information = tf.transpose(allocation_term) + content_term
     write_weight = write_gate * write_information
-    return write_weight
+    return write_weight # should return nx1
