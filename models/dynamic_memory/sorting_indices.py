3db1acc8a81094e4bf1a7f68717854cf095f5deb
Zan Huang <www.zandhuang@gmail.com>
2020-03-23 23:54:20 -0400

    Sorting Indicies still has an odd bug but several things are fixed and tested

--- models/dynamic_memory/sorting_indices.py
+++ models/dynamic_memory/sorting_indices.py
@@ -5,9 +5,12 @@ def sorting_indices(usage_vector):
     free_list_unsorted = usage_vector #Nx1 Matrix
     unsorted_memory_usage = usage_vector
 
-    free_list_sorted = tf.argsort(free_list_unsorted, axis = 0, direction = 'ASCENDING', stable=False)
-    free_list_sorted_index = tf.keras.backend.eval(free_list_sorted)
+    print(usage_vector)
 
-    ordered_memory_usage = tf.gather(unsorted_memory_usage, free_list_sorted_index, axis = 0)
+    free_list_sorted = tf.argsort(free_list_unsorted, axis = 0, direction = 'ASCENDING', stable=True)
+    free_list_sorted_index = tf.keras.backend.eval(free_list_sorted)
+    print(free_list_sorted_index)
+    ordered_memory_usage = tf.gather(unsorted_memory_usage, free_list_sorted_index, axis=0)
+    print(ordered_memory_usage)
 
     return ordered_memory_usage
