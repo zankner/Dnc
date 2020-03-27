daf17c48ba151eb0482e8879fcc72fba93dd8684
Zan Huang <www.zandhuang@gmail.com>
2020-03-27 16:41:22 -0400

    only the temporal linking section is left. I am definite that those are read weights and not write weights

--- models/controller.py
+++ models/controller.py
@@ -18,9 +18,9 @@ class Network(Model):
     self.weights_xi = Dense(memory_slot_size * read_heads + 3 * memory_slot_size + 5 * read_heads + 3) #previously g
     self.prev_usage_vector = tf.convert_to_tensor(np.random.random((memory_locations, 1)))
     self.read_weighting_prev = tf.convert_to_tensor(np.random.random((memory_locations, 1)))
-    self.prev_write_vector = tf.convert_to_tensor(np.random.random((memory_locations, 1)))
+    self.prev_write_weighting = tf.convert_to_tensor(np.random.random((memory_locations, 1)))
     self.memory_matrix = tf.convert_to_tensor(np.random.random((memory_locations, memory_slot_size)))
-
+    self.prev_precedence = tf.comvert_to_tensor()
 
   def call(self, input, read_weighting, training=False):
 
@@ -37,20 +37,28 @@ class Network(Model):
 
     #interface vector
     interface_vector = self.weights_xi(h)
-    r_keys, r_strengths, write_key, write_strength, \
-        e, w, free_gates, allocation_gate, write_gate, r_modes = tf.split(
+    read_keys, read_strengths, write_key, write_strength, \
+        erase_vector, write_vector, free_gates, allocation_gate, write_gate, read_modes = tf.split(
             interface_vector, [memory_slot_size*read_heads, read_heads, memory_slot_size, 1, memory_slot_size, memory_slot_size, read_heads, 1, 1, read_heads*3], 1)
     r_gates = [tf.split(r_gates, [r for r in range(read_heads)])]
 
 
-    #Accesing memory
-    write_vector, usage_vector = dynamic_memory(free_gates, self.read_weighting_prev, self.prev_usage_vector, self.prev_write_vector,
-                                                        allocation_gate, write_gate, self.memory_matrix, write_key, write_strength)
-    temporal_links, precedence = temporal_link(temporal_links, precedence, write_weightings)
+    #Dynamic memory------ Generating the write_weighting
 
+    write_weighting, usage_vector = dynamic_memory(free_gates, self.read_weighting_prev, self.prev_usage_vector, self.prev_write_weighting,
+                                                        allocation_gate, write_gate, self.memory_matrix, write_key, write_strength)
+
+    #Temporal Links and Generating the Read weighting
+    temporal_links, precedence = temporal_link(temporal_links, precedence, write_weightings)
+    forward_weight, backward_weight = temporal_weights(temporal_links, read_weighting_prev)
+    read_vector = 
+
+    # Write to Memory
+    self.memory_matrix = write(memory_matrix, write_weighting, erase_vector, write_vector)
     #update stored variables
     self.prev_usage_vector = usage_vector
     self.read_weighting_prev = read_weighting
     self.prev_write_vector = write_vector
+    self.prev_precendence = precedence
 
     return output_vector, interface_vector
--- models/dynamic_memory/dynamic_memory.py
+++ models/dynamic_memory/dynamic_memory.py
@@ -3,9 +3,9 @@ from tensorflow.keras.layers import Layer
 import memory_retention_vector, allocation_weighting, sorting_indices, usage_vector, write_weight_interpolation
 from models import content_lookup
 
-def dynamic_memory(free_gates, read_weighting_prev, prev_usage_vector, prev_write_vector, allocation_gate, write_gate, memory_matrix, key, key_strength):
+def dynamic_memory(free_gates, read_weighting_prev, prev_usage_vector, prev_write_weighting, allocation_gate, write_gate, memory_matrix, key, key_strength):
     retention_vector = memory_retention_vector.memory_retention_vector(free_gates, read_weighting_prev)
-    usage_vector = usage_vector.usage_vector(retention_vector, prev_usage_vector, prev_write_vector)
+    usage_vector = usage_vector.usage_vector(retention_vector, prev_usage_vector, prev_write_weighting)
     sorted_usage_vector = sorting_indices.sorting_indices(usage_vector)
     allocation_vector = allocation_weighting.allocation_weighting(sorted_usage_vector)
     content_vector = content_lookup(memory_matrix, key, key_strength)
