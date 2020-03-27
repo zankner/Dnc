b53b5db84b8c215e273e024b8cb986ef7d60b54b
Zan Huang <www.zandhuang@gmail.com>
2020-03-27 16:20:56 -0400

    structure finished and updated, need to fill in rest

--- models/controller.py
+++ models/controller.py
@@ -5,30 +5,27 @@ from tensorflow.keras.layers import (
 )
 from tensorflow.keras import Model
 from models.temporal_link import temporal_link
+from models.read_heads import get_read_vectors
+from models.write_heads import write
 
 
 class Network(Model):
-
-    #output dimensionality
-    n = 10
-
-    #interface dimensionality
-    g = 10
-
-  def __init__(self):
-    super(UNet, self).__init__()
+  def __init__(self, memory_locations, memory_slot_size, batch_size, input_dim, output_dim, interface_dim, read_heads):
     self.flatten = Flatten()
-    self.d1 = Dense(100, activation='relu')
-    self.weights_output = Dense(n)
-    self.weights_read = Dense(n)
-    self.weights_xi = Dense(g)
-    self.prev_usage_vector = 0
-    self.read_weighting_prev = 0
-    self.prev_write_vector = 0
+    self.d1 = Dense(input_dim, activation='relu')
+    self.weights_output = Dense(output_dim) #previously n
+    self.weights_read = Dense(output_dim)
+    self.weights_xi = Dense(memory_slot_size * read_heads + 3 * memory_slot_size + 5 * read_heads + 3) #previously g
+    self.prev_usage_vector = tf.convert_to_tensor(np.random.random((memory_locations, 1)))
+    self.read_weighting_prev = tf.convert_to_tensor(np.random.random((memory_locations, 1)))
+    self.prev_write_vector = tf.convert_to_tensor(np.random.random((memory_locations, 1)))
+    self.memory_matrix = tf.convert_to_tensor(np.random.random((memory_locations, memory_slot_size)))
 
 
-  def call(self, input, read_vectors, training=False):
+  def call(self, input, read_weighting, training=False):
 
+    #Here fetch the read read_vectors through read_weighting
+    read_vectors = get_read_vectors(self.memory_matrix, read_weighting)
     #feed forward
     read_vectors = self.flatten(read_vectors)
     input_vector = tf.concat(input, read_vectors)
@@ -40,22 +37,18 @@ class Network(Model):
 
     #interface vector
     interface_vector = self.weights_xi(h)
-    r_keys, r_strenghts, w_key, w_strength, \
-        e, w, r_gates, allocation, w_gate, r_models = tf.split(
-            interface_vector, [W*R, R, W, 1, W, W, R, 1, 1, R*3], 1)
-    r_gates = [tf.split(r_gates, [r for r in range(R)])]
+    r_keys, r_strengths, write_key, write_strength, \
+        e, w, free_gates, allocation_gate, write_gate, r_modes = tf.split(
+            interface_vector, [memory_slot_size*read_heads, read_heads, memory_slot_size, 1, memory_slot_size, memory_slot_size, read_heads, 1, 1, read_heads*3], 1)
+    r_gates = [tf.split(r_gates, [r for r in range(read_heads)])]
 
-    #fetching values from interface_vector
-    free_gates = ... more to fill
 
     #Accesing memory
     write_vector, usage_vector = dynamic_memory(free_gates, self.read_weighting_prev, self.prev_usage_vector, self.prev_write_vector,
-                                        allocation_gate, write_gate, content_vector,
-                                        memory_matrix, key, key_strength) #note I am falling alseep but please encode a mechanism
-                                        #for storing the previous usage vector and write vector.
-    temporal_links, precedence = temporal_link(temporal_links,
-        precedence, write_weightings)
+                                                        allocation_gate, write_gate, self.memory_matrix, write_key, write_strength)
+    temporal_links, precedence = temporal_link(temporal_links, precedence, write_weightings)
 
+    #update stored variables
     self.prev_usage_vector = usage_vector
     self.read_weighting_prev = read_weighting
     self.prev_write_vector = write_vector
--- models/dynamic_memory/dynamic_memory.py
+++ models/dynamic_memory/dynamic_memory.py
@@ -3,8 +3,7 @@ from tensorflow.keras.layers import Layer
 import memory_retention_vector, allocation_weighting, sorting_indices, usage_vector, write_weight_interpolation
 from models import content_lookup
 
-def dynamic_memory(free_gates, read_weighting_prev, prev_usage_vector, prev_write_vector, allocation_gate, write_gate,
-                    content_vector, memory_matrix, key, key_strength):
+def dynamic_memory(free_gates, read_weighting_prev, prev_usage_vector, prev_write_vector, allocation_gate, write_gate, memory_matrix, key, key_strength):
     retention_vector = memory_retention_vector.memory_retention_vector(free_gates, read_weighting_prev)
     usage_vector = usage_vector.usage_vector(retention_vector, prev_usage_vector, prev_write_vector)
     sorted_usage_vector = sorting_indices.sorting_indices(usage_vector)
